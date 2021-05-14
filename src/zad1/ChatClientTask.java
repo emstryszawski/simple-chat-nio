fd3c18f92613dd0ebaf1b3d661c3179ce64e40ac
streech <stryszawskiemil@gmail.com>
2021-05-14 21:11:09 +0200

    ChatClientTask done

--- src/zad1/ChatClientTask.java
+++ src/zad1/ChatClientTask.java
@@ -9,39 +9,32 @@ package zad1;
 
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 public class ChatClientTask extends FutureTask<ChatClient> {
 
-    private ChatClientTask(Callable<ChatClient> callable) {
+    private final ChatClient client;
+
+    private ChatClientTask(ChatClient client, Callable<ChatClient> callable) {
         super(callable);
+        this.client = client;
     }
 
     public static ChatClientTask create(ChatClient c, List<String> msgs, int wait) {
-        return new ChatClientTask(() -> {
-
+        return new ChatClientTask(c, () -> {
            c.login();
-           if (wait != 0)
-               Thread.sleep(wait);
+           if (wait != 0) Thread.sleep(wait);
            for (String msg : msgs) {
                c.send(msg);
-               if (wait != 0)
-                   Thread.sleep(wait);
+               if (wait != 0) Thread.sleep(wait);
            }
            c.logout();
-           if (wait != 0)
-               Thread.sleep(wait);
+           if (wait != 0) Thread.sleep(wait);
            return c;
         });
     }
 
     public ChatClient getClient() {
-        try {
-            return this.get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-            return null;
-        }
+        return client;
     }
 }
